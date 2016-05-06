fb56b251a419363a0171dce0e5d316af2405913b
Haoxiang <hx2185@columbia.edu>
2016-05-06 19:13:20 -0400

    first commit

---
+++ app/src/main/java/com/st/leighton/lingobarter/Login.java
@@ -0,0 +1,39 @@
+package com.st.leighton.lingobarter;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_login);
+
+        Button registerBtn = (Button) findViewById(R.id.hx_login_button_register);
+        Button retrieveBtn = (Button) findViewById(R.id.hx_login_button_forget_password);
+        if (registerBtn != null) {
+            registerBtn.setPaintFlags(registerBtn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        }
+        if (retrieveBtn != null) {
+            retrieveBtn.setPaintFlags(retrieveBtn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        }
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+}
